28295715cbc39cb6de747e8274cfbfb7ddc9b667
renanfigueoli09 <renanfigueoli@outlook.com>
2022-10-25 08:49:44 -0300

    teste

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require ("./src/app.js");
-const PORT = process.env.PORT || process.env.APP_URL; 
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, ()=>{
     console.log("Servidor funcionando em http://localhost:" + PORT)
 })
