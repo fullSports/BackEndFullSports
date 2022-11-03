bb52bbd99df0fc76da3f3cba9b1bb97eac318acd
renanfigueoli09 <renanfigueoli@outlook.com>
2022-11-03 10:57:01 -0300

    testes Imagens Produto

--- src/models/produto.js
+++ src/models/produto.js
@@ -8,27 +8,34 @@ const produtoSchema = new mongoose.Schema(
             type: String,
             require: true
         },
-        tipoProduto:{
-            type:String, 
-            required:true
-        },
-        corProduto:{
-            type:String, 
-            required:true
-        },
-        preco:{
-            type:String,
-            required:true
-        }, 
-        quantidade:{
-            type:Number, 
-            required:true
-        },
-        dataCadastro:{
-            type: String,
-            required:true
-        },
-        fornecedor:{type: mongoose.Schema.Types.ObjectId,ref:'fornecedores', required: true},
+        // tipoProduto:{
+        //     type:String, 
+        //     required:true
+        // },
+        // corProduto:{
+        //     type:String, 
+        //     required:true
+        // },
+        // preco:{
+        //     type:String,
+        //     required:true
+        // }, 
+        // quantidade:{
+        //     type:Number, 
+        //     required:true
+        // },
+        // dataCadastro:{
+        //     type: String,
+        //     required:true
+        // },
+        // fornecedor:{
+        //     type: mongoose.Schema.Types.ObjectId,ref:'fornecedores', 
+        //     required: true
+        // },
+        imagemProduto: {
+            type: Array,
+            required: true
+        }
     });
 const produto = mongoose.model("produtos",produtoSchema);
 module.exports=produto;
