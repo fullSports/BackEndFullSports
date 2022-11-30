67126f7863cf455b0877a31931d3805bf5cb7964
renanfigueoli <renanfigueoli@outlook.com>
2022-11-30 14:00:21 -0300

    test vercel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require ("./src/app.js");
+
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, ()=>{
     console.log("Servidor funcionando em http://localhost:" + PORT,"\nconectando com o banco...")
