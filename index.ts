24f1bbf373999299284ecb3d50af351772e875dc
renanfigueoli09 <renanfigueoli@outlook.com>
2022-12-06 01:42:08 -0300

    cnfig vercel

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import  {app} from './src/app/app'
-import {logger} from "./src/logger/index"
+import  {app} from './src/app/app';
+import {logger} from "./src/logger/index";
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, () => {    
     logger.info(`Servidor funcionando em http://localhost:${PORT} \nconectando com o banco...`)
 })
