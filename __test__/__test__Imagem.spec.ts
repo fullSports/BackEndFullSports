dd692c5de11b4fe0da985ada5930da193189ef69
renanfigueoli <renanfigueoli@outlook.com>
2022-12-08 16:37:30 -0300

    img

--- __test__/__test__Imagem.spec.ts
+++ __test__/__test__Imagem.spec.ts
@@ -1,8 +1,8 @@
 import app from "../src/app/app";
 import request from 'supertest'
 import logger from "../src/logger";
-import fs from 'fs'
-var img = fs.createReadStream("./tmp/testes-unitarios.png");
+// import fs from 'fs'
+// var img = fs.createReadStream("./tmp/testes-unitarios.png");
 describe("Teste do Backend do Fullsports - Deve Realizar todas as Rotas de Imagem", () => {
     const imagem = request(app);
     it("• Deve Execultar o método GET de todas as Imagens",async()=>{
@@ -13,7 +13,7 @@ describe("Teste do Backend do Fullsports - Deve Realizar todas as Rotas de Image
     it("• Deve Execultar o método POST de imagens", async () => {
         const CadastrarImagem = await imagem.post("/imagem")
         .send({
-            file: img
+            // file: img
         }) 
 
         console.log("ee"+ CadastrarImagem.body)
