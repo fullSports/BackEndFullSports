2e134551817e4e43b96634a2b29c1d717d761d0c
RenanFigueiredoEdGlobo <rfdoliveira@ogmaster.local>
2023-01-18 15:42:29 -0300

    reset image

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserModule } from "./users/users.module";
-import { ImagemModule } from "./imagem/imagem.module";
 var MongoUrl = "";
 if (process.env.ENV_AMB === "PROD") MongoUrl = process.env.mongoPROD;
 else if (process.env.ENV_AMB === "QA") MongoUrl = process.env.mongoQA;
 else MongoUrl = null;
 @Module({
-  imports: [MongooseModule.forRoot(MongoUrl), UserModule, ImagemModule],
+  imports: [MongooseModule.forRoot(MongoUrl), UserModule,],
   controllers: [AppController],
   providers: [AppService],
 })
