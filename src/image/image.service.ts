b1976a7ffb9390357536584ac16f8d291a60ed14
RenanFigueiredoEdGlobo <rfdoliveira@ogmaster.local>
2023-01-19 10:01:52 -0300

    (DELETE) image

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,32 +1,36 @@
 import { Model } from "mongoose";
 import { Body, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { ImageDocument,imagem, ImagesList } from "./Schema/image.schema";
+import { ImageDocument, imagem, ImagesList } from "./Schema/image.schema";
 
 @Injectable()
-export class ImageService{
-    constructor(@InjectModel(imagem.name) private readonly imageModel: Model<ImageDocument>){}
-    async registerImage(file){
-        const { originalname: name, size, key, location: url = "" } = file;
+export class ImageService {
+  constructor(
+    @InjectModel(imagem.name) private readonly imageModel: Model<ImageDocument>
+  ) {}
+  async registerImage(file) {
+    const { originalname: name, size, key, location: url = "" } = file;
 
-        const imagemPost = await this.imageModel.create({
-            name:name,
-            size:size,
-            key:key,    
-            url:url,
-        });
-        return imagemPost;
-    }
-    
-    async getImages():Promise<ImagesList[]>{
-      return this.imageModel.find().exec();
-    }
+    const imagemPost = await this.imageModel.create({
+      name: name,
+      size: size,
+      key: key,
+      url: url,
+    });
+    return imagemPost;
+  }
 
-    async getImageByID(id:string):Promise<ImagesList>{
-        return this.imageModel.findById({_id: id}).exec();
-    }
+  async getImages(): Promise<ImagesList[]> {
+    return this.imageModel.find().exec();
+  }
 
-    async deleteImage(id:string):Promise<ImagesList>{
-        return this.imageModel.findByIdAndDelete({_id:id}).exec();
-    }
+  async getImageByID(id: string): Promise<ImagesList> {
+    return this.imageModel.findById({ _id: id }).exec();
+  }
+
+  async deleteImage(id: string): Promise<ImagesList> {
+    const deleteImage = await this.imageModel.findById({ _id: id });
+    await deleteImage.remove();
+    return deleteImage;
+  }
 }
