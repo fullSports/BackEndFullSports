580d7fbedb930a0c3bf984c4140c308305ace2b8
RenanFigueiredoEdGlobo <rfdoliveira@ogmaster.local>
2023-01-19 18:25:42 -0300

    tratamento de erro (DELETE) img local

--- src/image/Schema/image.schema.ts
+++ src/image/Schema/image.schema.ts
@@ -1,49 +1,70 @@
-
-import { HydratedDocument } from "mongoose"
+import { Logger } from "@nestjs/common";
+import { HydratedDocument } from "mongoose";
 export type ImageDocument = HydratedDocument<typeof ImagemSchema>;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 const AWS = require("aws-sdk");
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 const s3 = new AWS.S3({
-    accessKeyId: process.env.KEY_ID,
-    secretAccessKey: process.env.ACCESS_KEY,
-    region: process.env.REGIONAWS
-})
+  accessKeyId: process.env.KEY_ID,
+  secretAccessKey: process.env.ACCESS_KEY,
+  region: process.env.REGIONAWS,
+});
 export const ImagemSchema = new mongoose.Schema({
-    name: String,
-    size: Number,
-    key: String,
-    url: String,
-    createAt: {
-        type: Date,
-        default: Date.now,
-    }
+  name: String,
+  size: Number,
+  key: String,
+  url: String,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
-ImagemSchema.pre('save',function(){
-   if(!this.url) {
-        this.url=`http://localhost:${process.env.PORT}/files/${this.key}`;
-   }
+ImagemSchema.pre("save", function () {
+  if (!this.url) {
+    this.url = `http://localhost:${process.env.PORT}/files/${this.key}`;
+  }
 });
-ImagemSchema.pre('remove',function(){
-    if(process.env.STORAGE_TYPE === 's3'){
-        return s3.deleteObject({
-            Bucket: process.env.BUCKET,
-            Key: this.key
-        }).promise()
-    }else{
-        return promisify(
-        fs.unlink)(path.resolve(__dirname,"..","..","..","tmp","uploads",this.key)
-        );
-    };
+ImagemSchema.pre("remove", function () {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET,
+        Key: this.key,
+      })
+      .promise();
+  } else {
+    fs.open(
+      path.resolve(__dirname, "..", "..", "..", "tmp", "uploads", this.key),
+      (err, fd) => {
+        if (!err) {
+          console.error("myfile already exists");
+          return promisify(fs.unlink)(
+            path.resolve(
+              __dirname,
+              "..",
+              "..",
+              "..",
+              "tmp",
+              "uploads",
+              this.key
+            )
+          );
+        } else return null;
+      }
+    );
+    // promisify(fs.unlink)(
+    //     path.resolve(__dirname, "..", "..", "..", "tmp", "uploads", this.key)
+    //   );
+  }
 });
 export const imagem = mongoose.model("imagem", ImagemSchema);
 export class ImagesList {
-    name: String;
-    size: Number;
-    key: String;
-    url: String;
-    createAt: Object;
+  name: String;
+  size: Number;
+  key: String;
+  url: String;
+  createAt: Object;
 }
