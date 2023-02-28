0156156a72e31e232b505cda0d49f8211a15a209
RenanFigueiredoEdGlobo <rfdoliveira@ogmaster.local>
2023-02-28 16:37:42 -0300

    test2

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 const { resolve } = require("path");
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors: true});
+  const app = await NestFactory.create(AppModule);
+  const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials: false,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
