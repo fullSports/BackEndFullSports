9c7fcb3be4ba33f30305071e4bb0bbad8b300347
RenanFigueiredoEdGlobo <rfdoliveira@ogmaster.local>
2023-02-28 16:58:59 -0300

    testestete

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ const { resolve } = require("path");
 const express = require("express");
 const path = require("path");
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header("Access-Control-Allow-Headers", '*');
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("'Content-Type'", "'multipart/form-data'");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors())
-    next()
-  })
+  const app = await NestFactory.create(AppModule,{cors:true});
+  // app.use((req: Request, res: Response, next: NextFunction) => {
+  //   res.header("Access-Control-Allow-Headers", '*');
+  //   res.header("Access-Control-Allow-Origin", '*');
+  //   res.header("'Content-Type'", "'multipart/form-data'");
+  //   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  //   app.use(cors())
+  //   next()
+  // })
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
