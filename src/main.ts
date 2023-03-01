70a66cdb681e96dc372a2c6bb8afbe4628988358
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 18:28:32 -0300

    test vercel 6

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ const path = require("path");
 const cors = require("cors")
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
     allowedHeaders: '*',
     origin: '*',
