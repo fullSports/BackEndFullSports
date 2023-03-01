d6c30a86cf5861ad76f8888e7ff8d2848a4824c3
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 11:40:45 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
     { 
-      origin: ['http://localhost:300', 'https://www.google.com'],
+      origin: ['http://localhost:3000', 'https://www.google.com'],
       methods: ['POST', 'PUT', 'DELETE', 'GET']
     }
   );
