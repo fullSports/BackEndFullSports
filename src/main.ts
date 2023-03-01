979a062f24714db33de06617ce1585088a59cb4e
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 10:27:53 -0300

    ;(

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,7 @@ const { resolve } = require("path");
 const express = require("express");
 const path = require("path");
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  })
+  const app = await NestFactory.create(AppModule,{cors:true});
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
