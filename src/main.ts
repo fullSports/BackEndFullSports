7c0df7c53a38e5d095d63eb629d75ea1d765c523
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 11:32:56 -0300

    test

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,7 @@ const path = require("path");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req:Request, res:Response, next:NextFunction)=>{
-    res.header("Access-Control-Allow-Headers", '*');
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("'Content-Type'", "'multipart/form-data'");
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors())
-    next()
-})
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
