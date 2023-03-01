febd41343d374b25d9ab91f9074ba79573d4af2c
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 17:00:39 -0300

    test cors 2

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,19 @@ const path = require("path");
 const cors = require("cors")
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors({
+    
+  //   origin: '*',
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  //   allowedHeaders: ['Authorization', 'Content-Type'],
+  //   exposedHeaders: ['Content-Length'],
+  //   credentials: true,
+  //   maxAge: 3600,
+  // });
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Authorization', 'Content-Type'],
-    exposedHeaders: ['Content-Length'],
+    allowedHeaders: '*',
+    origin: '*',
     credentials: true,
-    maxAge: 3600,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(
