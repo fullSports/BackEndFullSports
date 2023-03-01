3855808a7e079f768780cfe312ba6eb10238e8f6
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-01 11:40:30 -0300

    test origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,12 @@ const path = require("path");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors(
+    { 
+      origin: ['http://localhost:300', 'https://www.google.com'],
+      methods: ['POST', 'PUT', 'DELETE', 'GET']
+    }
+  );
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
