2f53e93aac4803835e92791ff19dbd6db3e231c7
Renan Figueiredo de Oliveira <78419902+renanfigueoli09@users.noreply.github.com>
2023-03-01 09:59:54 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const { resolve } = require("path");
 const express = require("express");
 const path = require("path");
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:false});
+  const app = await NestFactory.create(AppModule,{cors:true});
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
