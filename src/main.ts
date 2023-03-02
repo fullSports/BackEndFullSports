0efdcd7dbb1e2346f0b96ed924c8e3192622bc4a
renanfigueoli09 <renanfigueoli@outlook.com>
2023-03-02 08:21:10 -0300

    test cors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
+import { NestApplication, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
@@ -8,7 +8,8 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors")
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     "/files",
@@ -21,22 +22,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("document", app, document);
-
-  var whitelist = ['http://localhost:3000', 'https://www.fullsports.dev.br'];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log("allowed cors for:", origin)
-        callback(null, true)
-      } else {
-        console.log("blocked cors for:", origin)
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-    credentials: true,
-  });
   await app.listen(process.env.PORT);
   app.use("/ducument", express.static(resolve(__dirname, "./build")));
   Logger.log(`server on in http://localhost:${process.env.PORT}`);
