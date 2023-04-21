8020a07bf8e82bdb548c9989549a364f701aedf5
RenanFigueiredoEdglobo <rfdoliveira@ogmaster.local>
2023-04-21 17:34:14 -0300

    test

--- src/user.spec.ts
+++ src/user.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
-import { AppModule } from "./../src/app.module";
+import { AppModule } from "./app.module";
 describe("Users", () => {
   let app: INestApplication;
   beforeEach(async () => {
