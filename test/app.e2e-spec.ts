9401118acdf260660779848e1fea7ea47455aceb
RenanFigueiredoEdglobo <renanfigueoli@outlook.com>
2023-05-06 11:06:03 -0300

    update app

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
-import { AppModule } from "./../src/app.module";
-
+import { AppService } from "src/app.service";
+import { AppController } from "src/app.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+const urlConfig = require("./globalConfig.json");
 describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MongooseModule.forRoot(urlConfig.mongoUri)],
+      controllers: [AppController],
+      providers: [AppService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
