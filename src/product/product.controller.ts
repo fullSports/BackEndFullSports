a2b2c3a54c38c9f83b5f2fad172ec2f87c3613a0
GabrielGozzi <biel20032010@gmail.com>
2023-06-06 14:29:53 -0300

    adicionando authGuard em todas as rotas

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,21 +6,23 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { updateProductDTO } from "./dto/updateProduct.dto";
 import { ProductServices } from "./product.service";
 import { Product } from "./Schema/product.schema";
+import { AuthGuard } from "@nestjs/passport";
 @Controller()
 @ApiTags("Products")
 export default class ProductController {
   constructor(private readonly productService: ProductServices) {}
-
+  @UseGuards(AuthGuard('jwt'))
   @Get("/listar-produtos")
   async ListProduct(): Promise<Product[]> {
     return await this.productService.listProducts();
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post("/cadastrar-produto")
   async CreateProduct(@Body() creatProduct: Product) {
     creatProduct["dataCadastro"] = new Date().toISOString();
@@ -33,12 +35,12 @@ export default class ProductController {
       messagem: "produto cadastrado com sucesso",
     };
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Get("/listar-produto/:id")
   async SearchProductById(@Param("id") id: string): Promise<Product> {
     return this.productService.searchProductId(id);
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Put("/atualizar-produto/:id")
   async updateProductId(
     @Param("id") id: string,
@@ -53,6 +55,7 @@ export default class ProductController {
       messagem: "produto atualizado com sucesso",
     };
   }
+  @UseGuards(AuthGuard('jwt'))
   @Delete("/deletar-produto/:id")
   async deleteProduct(@Param("id") id: string) {
     await this.productService.deleteProduct(id);
@@ -60,7 +63,7 @@ export default class ProductController {
       messagem: "Produto deletado com sucesso ",
     };
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Get("/buscar-produto/:busca")
   async searchProducts(@Param("busca") search: string) {
     return await this.productService.searchProducts(search);
