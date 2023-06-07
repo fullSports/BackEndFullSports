9f485c7c3650c7471515adb81fffafe0d1b8e7db
renanfigueoli09 <renanfigueoli@outlook.com>
2023-06-06 21:34:54 -0300

    atualizando tempo de expiração do token

--- src/auth/config/jwt.config.ts
+++ src/auth/config/jwt.config.ts
@@ -1,5 +1,5 @@
 import { JwtModuleOptions } from '@nestjs/jwt';
 export const jwtConfig: JwtModuleOptions = {
     secret: 'teste', // Defina sua própria chave secreta
-    signOptions: { expiresIn: '2h' }, // Define a expiração do token JWT
-};
+    signOptions: { expiresIn: '3min' }, // Define a expiração do token JWT
+};  
