1c533f3913cb52cbebf2bf763afc6c63f53189f6
renanfigueoli09 <renanfigueoli@outlook.com>
2023-10-01 17:47:02 -0300

    ajuste no cache na roda de atualziar-produto/:id

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -77,6 +77,7 @@ export default class ProductController {
       updateProduct
     );
     this.queueCacheService.addItem(RequestsEnum.product);
+    this.queueCacheService.addItem(`${RequestsEnum.product}-${id}`);
     return {
       product: updateProductId,
       messagem: "produto atualizado com sucesso",
