7ee0d50c11c029d811162e939ce3a4bffdaca76d
renanfigueoli09 <renanfigueoli@outlook.com>
2023-10-01 18:52:18 -0300

    implementando cache nas rodas de pedidos

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,8 +1,11 @@
 import {
   Body,
+  CACHE_MANAGER,
   Controller,
   Delete,
   Get,
+  Inject,
+  NotAcceptableException,
   Param,
   Post,
   UseGuards,
@@ -11,15 +14,23 @@ import { ApiTags } from "@nestjs/swagger";
 import { OrderService } from "./order.service";
 import { Order } from "./Schema/order.schema";
 import { AuthGuard } from "@nestjs/passport";
+import { QueueCacheService } from "src/queues/jobs/queue.cache.service";
+import { Cache } from "cache-manager";
+import { RequestsEnum } from "src/queues/enum/request.enum";
 @Controller()
 @ApiTags("Orders")
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    @Inject(CACHE_MANAGER) private readonly cache: Cache,
+    private readonly queueCacheService: QueueCacheService
+  ) {}
 
   @Post("/realizar-pedido")
   @UseGuards(AuthGuard("jwt"))
   async RegisterOrder(@Body() createOrder: Order) {
     const RegisterOrder = await this.orderService.RegisterOrder(createOrder);
+    this.queueCacheService.addItem(RequestsEnum.order);
     return {
       messagem: "pedido realizado com suceeso",
       order: RegisterOrder,
@@ -30,15 +41,27 @@ export class OrderController {
   @Get("/listar-pedidos")
   @UseGuards(AuthGuard("jwt"))
   async ListOrders(): Promise<Order[]> {
-    const listOrders = await this.orderService.ListOrders();
-    return listOrders;
+    const orders_cache = await this.cache.get(RequestsEnum.order);
+    if (orders_cache) {
+      return orders_cache;
+    } else {
+      const orders = await this.orderService.ListOrders();
+      await this.cache.set(RequestsEnum.order, orders);
+      return orders;
+    }
   }
 
   @Get("/listar-pedido/:id")
   @UseGuards(AuthGuard("jwt"))
   async ListOrderByID(@Param("id") id: string) {
-    const listOrder = await this.orderService.searchIdOrder(id);
-    return listOrder;
+    const orderId_cache = await this.cache.get(`${RequestsEnum.order}-${id}`);
+    if (orderId_cache) {
+      return orderId_cache;
+    } else {
+      const orderId = await this.orderService.searchIdOrder(id);
+      await this.cache.set(`${RequestsEnum.order}-${id}`);
+      return orderId;
+    }
   }
 
   // @Put("/atualizar-pedido/:id")
@@ -54,6 +77,13 @@ export class OrderController {
   @UseGuards(AuthGuard("jwt"))
   async DeleteOrder(@Param("id") id: string) {
     const deleteOrder = await this.orderService.deleteOrder(id);
-    return deleteOrder;
+    if (deleteOrder) {
+      this.queueCacheService.addItem(RequestsEnum.order);
+      return {
+        messagem: "pedido cancelado com sucesso",
+      };
+    } else {
+      throw new NotAcceptableException();
+    }
   }
 }
